2ff0c2540411b8755d22eddc295681eb876d7986
sarpscode <sarponge60@gmail.com>
2022-05-11 02:11:15 -0700

    finish

--- Atm.py
+++ Atm.py
@@ -7,7 +7,7 @@ security_code = 2022
 password_attempt = 1
 input_password = ''
 is_password_lock = False
-your_amount = 500
+your_amount = 1000
 
 while security_code != input_password:
     if password_attempt <= 3:
@@ -36,6 +36,66 @@ while True:
     elif inquire_option == 2:
         print("Select amount")
         print("1.  50 , 2. 100 , 3. 150 , 4. 300 , 5. 500")
-        withdrawal_option = input("Enter amount option: ")
+        withdrawal_option = int(input("Enter amount option: "))
     
+        if withdrawal_option ==1:
+            print("1.  Confirm Or 2.  Abort")
+            confirm1 = int(input())
+            if confirm1 == 1:
+                currentCash = your_amount - int(50)
+                print("Current balance: " + str(currentCash))
+                print("Thank You")
+                break
+            elif confirm1 == 2:
+                print("The process has  been successfully aborted..... Thank You!!!")
+                break
+               
+        elif withdrawal_option ==2:
+            print("1.  Confirm Or 2.  Abort")
+            confirm1 = int(input())
+            if confirm1 == 1:
+                currentCash = your_amount - int(100)
+                print("Current balance: " + str(currentCash))
+                print("Thank You")
+                break
+            elif confirm1 == 2:
+                print("The process has been successfully aborted..... Thank You!!!")
+                break
+                
+        elif withdrawal_option ==3:
+            print("1.  Confirm Or 2.  Abort")
+            confirm1 = int(input())
+            if confirm1 == 1:
+                currentCash = your_amount - int(150)
+                print("Current balance: " + str(currentCash))
+                print("Thank You")
+                break
+            elif confirm1 == 2:
+                print("The process has been successfully aborted..... Thank You!!!")
+                break
     
+        elif withdrawal_option ==4:
+            print("1.  Confirm Or 2.  Abort")
+            confirm1 = int(input())
+            if confirm1 == 1:
+                currentCash = your_amount - int(300)
+                print("Current balance: " + str(currentCash))
+                print("Thank You")
+                break
+            elif confirm1 == 2:
+                print("The process been has successfully aborted..... Thank You!!!")
+                break
+                
+        elif withdrawal_option ==5:
+            print("1.  Confirm Or 2.  Abort")
+            confirm1 = int(input())
+            if confirm1 == 1:
+                currentCash = your_amount - int(500)
+                print("Current balance: " + str(currentCash))
+                print("Thank You")
+                break
+            elif confirm1 == 2:
+                print("The process has been successfully aborted..... Thank You!!!")
+                break
+        else:
+            print("Invalid digit")
